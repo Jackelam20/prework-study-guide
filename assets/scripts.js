4d66881ff6015641f1ac82c840d8551b5a3ad7f9
jack <jackelam20@gmail.com>
2023-06-02 21:12:36 -0500

    added function and for loop to JavaScript for preowkr-study-guide

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,17 +1,25 @@
 var topics = ["HTML", "CSS", "Git", "JavaScript"];
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
+function listTopiocs() {
 for (var x = 0; x < topics.length; x++) {
     console.log(topics[x]);
+ }
 }
-/*
-if (topic === 'HTML') {
+
+function selectTopic() {
+if (randomTopic === 'HTML') {
     console.log ("Let's study HTML!");
-}  else if (topic === 'CSS') {
+ }  else if (randomTopic === 'CSS') {
     console.log ("Let's study CSS!");
-}  else if (topic === 'Git') {
+ }  else if (randomTopic === 'Git') {
     console.log ("Let's study Git!" );
-} else if (topic === 'JavaScript'){
+ } else if (randomTopic === 'JavaScript'){
     console.log ("Let's syudy JavaScript!");
-}else{
+ }else{
     console.log ("please Try Again!");
+ }
 }
-*/
+console.log('Here are the topics we learned throught Prework');
+listTopiocs()
+console.log('Which topic should we stuidy first?');
+selectTopic()
